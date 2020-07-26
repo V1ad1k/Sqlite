0bb06dbc7ba0829b8534c7196e5ad0662b669723
V1ad1k <gavryliuk1409@gmail.com>
2020-07-26 19:30:17 +0200

    Project Euler #9

--- qwe.py
+++ qwe.py
@@ -1,1 +1,12 @@
-import sqlite3
+
+def calc(z):
+    sum1 = 0
+    sum2 = 0
+    for i in range(1,z+1):
+        sum1 += i
+    sum1 = sum1**2
+    for i in range(1,z+1):
+        sum2 += i*i
+    result = sum1 - sum2
+    return result
+print(calc(100))
